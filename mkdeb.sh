d83dfb120a7f9c3ff710166c6220a8e234497c49
Samer Masterson <samer@samertm.com>
2016-02-22 18:09:51 -0800

    update mkdeb

--- mkdeb.sh
+++ mkdeb.sh
@@ -2,4 +2,4 @@
 
 TIMESTAMP=$(python -c "import time; print time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))")
 
-fpm --verbose --debug --prefix /usr -C pkg-out -s dir -t deb -n "node" -v "v0.12.9-$TIMESTAMP" bin include lib share
+fpm --verbose --debug --prefix /usr -C pkg-out -s dir -t deb -n "node" -v "0.12.9-$TIMESTAMP" bin include lib share
